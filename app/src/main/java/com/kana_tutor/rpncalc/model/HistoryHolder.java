544640eb3cc43bd78b681fbde741cea51e6c35a8
sjs <sjs@sonic.net>
2020-10-27 12:41:28 -0700

    initial

---
+++ app/src/main/java/com/kana_tutor/rpncalc/model/HistoryHolder.java
@@ -0,0 +1,13 @@
+package com.kana_tutor.rpncalc.model;
+
+
+public class HistoryHolder {
+
+    public String operation;
+    public String result;
+
+    public HistoryHolder(String o, String r){
+        this.operation = o;
+        this.result = r;
+    }
+}
